8594013b718ef5ca6b87049d491c56eb8ec0bd88
Jonathan Fernandes <jhfernan@mail.usf.edu>
2023-09-21 12:58:51 +0800

    Added some input and discussed using delta time

--- assets/models/player.py
+++ assets/models/player.py
@@ -1,6 +1,4 @@
 import pygame as py
-SCREEN_W = py.display.get_window_size()[0]
-SCREEN_H = py.display.get_window_size()[1]
 
 
 class Player(py.sprite.Sprite):
@@ -13,4 +11,25 @@ class Player(py.sprite.Sprite):
         self.image = py.image.load("assets/graphics/player_ship.png").convert_alpha()
 
         # It needs a rect
+        SCREEN_W = py.display.get_window_size()[0]
+        SCREEN_H = py.display.get_window_size()[1]
         self.rect = self.image.get_rect(center=(SCREEN_W / 2, SCREEN_H - 80))
+
+        # Vector based movement
+        self.position = py.math.Vector2(self.rect.center)
+        self.direction = py.math.Vector2()
+        self.speed = 250
+
+    def input(self):
+        keys = py.key.get_pressed()
+
+        if keys[py.K_w]:
+            self.direction.y = -1
+        else:
+            self.direction.y = 0
+
+    def move(self, delta_time):
+        self.position += self.speed * self.direction * delta_time
+
+    def update(self):
+        self.input()
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 import pygame as py
 
+from assets.models.player import Player
+
 # Screen Settings
 py.init()
 screen_dimensions = (854, 480)
@@ -13,6 +15,10 @@ py.display.set_caption("Pygame 3 Game")
 # Sprites and Graphics setup
 bg = py.image.load("assets/graphics/bg_space.png").convert()
 
+ship_group = py.sprite.GroupSingle()
+
+player = Player(group=ship_group)
+
 # Game Loop
 while True:
     # Event Loop
@@ -24,7 +30,10 @@ while True:
     # Time, Updates, Graphics
     delta_time = clock.tick(120) / 1000
 
+    ship_group.update()
+
     screen.blit(bg, (0, 0))
+    ship_group.draw(screen)
 
     # Screen Update
     py.display.update()
