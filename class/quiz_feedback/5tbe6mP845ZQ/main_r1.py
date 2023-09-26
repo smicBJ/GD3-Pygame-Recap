85be4430bbc8d0b8303a03a1d720ff71834b1126
Jonathan Fernandes <jhfernan@mail.usf.edu>
2023-09-26 15:09:41 +0800

    Added more notes for a revised quiz

--- class/quiz_feedback/5tbe6mP845ZQ/main_r1.py
+++ class/quiz_feedback/5tbe6mP845ZQ/main_r1.py
@@ -4,19 +4,19 @@ import pygame as py
 
 from player_r1 import Player
 
-# -1: You are missing the import of the Player class here
+# ✅: You are missing the import of the Player class here
 # for the above issue, I added the missing import of player
 
 # Put spaces between your import, variables and function definitions
 #initializing game
 py.init()
 screen_dimensions = (720,480)
-# -1: Do not put spaces between methods
+# ✅: Do not put spaces between methods
 #deleted the spaces
 screen = py.display.set_mode(screen_dimensions)
 clock = py.time.Clock()
 
-# -1: Why are you flipping the display here? This line seems irrelevant
+# ✅: Why are you flipping the display here? This line seems irrelevant
 #py.display.flip()
 # for above issue, I removed the line. I wanted to refresh the page at the time.
 clock.tick(60)
@@ -24,12 +24,12 @@ clock.tick(60)
 #misc setup
 py.display.set_caption("trial")
 
-# -1: You are missing the group being created here
+# ✅: You are missing the group being created here
 # created the group, name as "characters".
 characters = py.sprite.GroupSingle()
 
 player = Player(group=characters)
-# -1: You should create the player instance here and add it to the group
+# ✅: You should create the player instance here and add it to the group
 #created the missing player
 # added the player into the group "characters"
 
@@ -37,15 +37,16 @@ player = Player(group=characters)
 bg = py.image.load("Unit 2 Quiz - assets/map.png").convert()
 
 #game loop
+# -1: You are missing the colon after the true, which should be with a capital T
 while true
     #events
     for event in py.event.get():
         if event.type == py.QUIT:
             py.quit()
             sys.exit()
+    # -1: This loop is missing the group update and draw method being called and then the screen being updated
 
-
-
+# -1: You do not need two while loops so this second one will be irrelevant
 while true
     delta_time = clock.tick(100)/1200
     screen.blit(bg, (0, 0))
--- class/quiz_feedback/5tbe6mP845ZQ/player_r1.py
+++ class/quiz_feedback/5tbe6mP845ZQ/player_r1.py
@@ -1,24 +1,25 @@
-# -1: Missing pygame import
+# ✅: Missing pygame import
 #added the missing import
 import pygame as py
 
 
 class Player(py.sprite.Sprite):
-    # -1: Missing the self argument
+    # ✅: Missing the self argument
     #change to self argument
     #def __init__(player_img, player_rect, player_pos)
     def __init__(self, group):
-        # -1: Use the parent call instead
+        # ✅: Use the parent call instead
         # ok, change to using parent call.
         super().__init__(group)
         #added a super, to link it to parent.
         # py.sprite.Sprite.__init__(self)
-        # -1: Improper image import
+        # ✅: Improper image import
         #added py.image.load, and .convert alpha
         self.image = py.image.load("Unit 2 Quiz - assets/0.png").convert_alpha()
-        # -1: Improper rect creation
+        # ✅: Improper rect creation
         #self.rect = player_rect(0)
         # change to proper self.image.get_rect
+        # -1: You forgot to create the variables before using them, so first create SCREEN_W and SCREEN_H
         self.rect = self.image.get_rect(center=(SCREEN_W / 2, SCREEN_H / 2))
         # self.rect.topleft = init_pos
 
