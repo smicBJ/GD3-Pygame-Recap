21a2bdf6f8062a1f7b14ef40c2791a7a63f6dc51
eThAN02010506 <144652834+eThAN02010506@users.noreply.github.com>
2023-10-09 22:37:24 +0800

    Redo 2

---
+++ class/quiz_feedback/5tbe6mP845ZQ/main_r2.py
@@ -0,0 +1,59 @@
+import sys
+
+import pygame as py
+
+from player_r1 import Player
+
+# ✅: You are missing the import of the Player class here
+# for the above issue, I added the missing import of player
+
+# Put spaces between your import, variables and function definitions
+# initializing game
+py.init()
+screen_dimensions = (720,480)
+# ✅: Do not put spaces between methods
+# deleted the spaces
+screen = py.display.set_mode(screen_dimensions)
+clock = py.time.Clock()
+
+# ✅: Why are you flipping the display here? This line seems irrelevant
+# py.display.flip()
+# for above issue, I removed the line. I wanted to refresh the page at the time.
+clock.tick(60)
+
+# misc setup
+py.display.set_caption("trial")
+
+# ✅: You are missing the group being created here
+# created the group, name as "characters".
+characters = py.sprite.GroupSingle()
+
+player = Player(group=characters)
+# ✅: You should create the player instance here and add it to the group
+# created the missing player
+# added the player into the group "characters"
+
+# spirit and graphic set up
+bg = py.image.load("Unit 2 Quiz - assets/map.png").convert()
+
+# game loop
+# -1: You are missing the colon after the true, which should be with a capital T
+# added the colon and change true to True
+while True:
+    #events
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            py.quit()
+            sys.exit()
+    # -1: This loop is missing the group update and draw method being called and then the screen being updated
+    # added the update
+    player.update()
+
+    screen.blit(bg, (0, 0))
+    player.draw(screen)
+# -1: You do not need two while loops so this second one will be irrelevant
+# while true
+# removed the second while loop
+    delta_time = clock.tick(100)/1200
+    screen.blit(bg, (0, 0))
+
---
+++ class/quiz_feedback/5tbe6mP845ZQ/player_r2.py
@@ -0,0 +1,28 @@
+# ✅: Missing pygame import
+# added the missing import
+import pygame as py
+
+
+class Player(py.sprite.Sprite):
+    # ✅: Missing the self argument
+    # change to self argument
+    # def __init__(player_img, player_rect, player_pos)
+    def __init__(self, group):
+        # ✅: Use the parent call instead
+        # ok, change to using parent call.
+        super().__init__(group)
+        # added a super, to link it to parent.
+        # py.sprite.Sprite.__init__(self)
+        # ✅: Improper image import
+        # added py.image.load, and .convert alpha
+        self.image = py.image.load("Unit 2 Quiz - assets/0.png").convert_alpha()
+        # ✅: Improper rect creation
+        # self.rect = player_rect(0)
+        # change to proper self.image.get_rect
+        # -1: You forgot to create the variables before using them, so first create SCREEN_W and SCREEN_H
+        # create the variables:
+        SCREEN_W = py.display.get_window_size()[0]
+        SCREEN_H = py.display.get_window_size()[1]
+        self.rect = self.image.get_rect(center=(SCREEN_W / 2, SCREEN_H - 100))
+        # self.rect.topLeft = init_pos
+
