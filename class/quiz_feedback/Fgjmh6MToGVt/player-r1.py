3d0b45b410b09a4af2e2eba37e4f551146e3bb0e
smicBJ <144313244+smicBJ@users.noreply.github.com>
2023-10-11 09:41:28 +0800

    Update player-r1.py
    Gave feedback

--- class/quiz_feedback/Fgjmh6MToGVt/player-r1.py
+++ class/quiz_feedback/Fgjmh6MToGVt/player-r1.py
@@ -2,19 +2,19 @@ import pygame
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
-        # -1: Use the parent class otherwise the inheritance is irrelevant
+        # ✅: Use the parent class otherwise the inheritance is irrelevant
         # INIT FROM PARENT CLASS
         super(Player, self).__init__()
         #pygame.sprite.Sprite.__init__(self)
-        # -1: The following lines can be condensed into one line and needs to properly import the image
+        # ✅: The following lines can be condensed into one line and needs to properly import the image
         self.image = pygame.image.load("unit2_quiz_assets/assets/0.png").convert_alpha()
         # REMOVED CODE FOR RESIZING IMAGE
         # self.image_size = self.rootimage.get_size()
         # self.image = pygame.transform.scale(self.rootimage, (int(self.image_size[0]/5), int(self.image_size[1]/5)))
-        # -1: Missing rect
+        # ✅: Missing rect
         self.rect = self.image.get_rect()
 
-    # -1: The following 2 methods are not needed for this assignment
+    # ✅: The following 2 methods are not needed for this assignment
     # REMOVED UNNEEDED METHODS
     # def update(self, dt):
     #     key = pygame.key.get_pressed()
