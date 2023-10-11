55980be63b3062565358a6a14c759767dc71205d
Oscar071130 <144652929+Oscar071130@users.noreply.github.com>
2023-10-11 13:51:58 +0800

    Add files via upload
    Added revisiona

---
+++ class/quiz_feedback/uCI6MH2DQteo/main_r1.py
@@ -0,0 +1,18 @@
+import pygame
+# -1: No player class import
+pygame.init()
+dis=pygame.display.set_mode((400,300))
+# -1: The clock is not properly created
+# -1: This line is out of place
+pygame.display.update()
+# Do not use different characters when typing code, as these are not recognized as parenthesis
+pygame.display.set_caption("Snake game by Intellipnat")
+game_over=False
+while not game_over:
+    # -1: You are missing the colon here and are not properly checking for the event
+    for event in pygame.event.get()
+        print(event)  #prints out all the actions thta take place
+        # on the screen
+
+        pygame.quit()
+        quit()
