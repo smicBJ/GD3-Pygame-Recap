7097a125983b090830b4cb9f25628828e76e1c1e
smicBJ <144313244+smicBJ@users.noreply.github.com>
2023-10-11 09:45:33 +0800

    Update main-r1.py
    Still some issues present

--- class/quiz_feedback/Fgjmh6MToGVt/main-r1.py
+++ class/quiz_feedback/Fgjmh6MToGVt/main-r1.py
@@ -1,4 +1,4 @@
-# -1: You have an import for time that is not being used here, and you are mixing import types
+# ✅: You have an import for time that is not being used here, and you are mixing import types
 
 #REMOVED OLD IMPORT
 #import pygame, time, player
@@ -14,13 +14,13 @@ pygame.display.set_caption("Unit 2 Quiz")
 clock = pygame.time.Clock()
 background_img = pygame.image.load("unit2_quiz_assets/assets/map.png").convert()
 background_img = pygame.transform.scale(background_img, (396, 480))
-# -1: This blit must be done in the game loop
+# ✅: This blit must be done in the game loop
 #BLIT MOVED TO GAME LOOP
 #screen.blit(background_img, (0, 0))
 #SPRITE GROUP RENAMED FROM "PLAYERS" TO "SPRITES" SINCE THERE WILL ONLY BE ONE PLAYER
 sprites = pygame.sprite.Group()
-# -1: Missing the instance of the player sprite being created
-# -1: Missing adding the player sprite to the group
+# ✅: Missing the instance of the player sprite being created
+# ✅: Missing adding the player sprite to the group
 #CREATING & ADDING PLAYER INSTANCE TO SPRITE GROUP MOVED HERE FROM GAME LOOP
 player = Player()
 sprites.add(player)
@@ -35,7 +35,7 @@ while running:
 
     # -1: The first thing in the changes section should be the creation of delta time
 
-    # -1: This is not only not the right place to create an instance and add it to the group, but also the wrong cond.
+    # ✅: This is not only not the right place to create an instance and add it to the group, but also the wrong cond.
     # SPRITE INSTANCE MOVED TO PROPER LOCATION & REMOVED
     # mouse = pygame.mouse.get_pressed()
     # if mouse[0]:
@@ -46,10 +46,15 @@ while running:
 
     # flip() the display to put your work on screen
     screen.blit(background_img, (0, 0))
+
+    # -1: You should be using the group method to handle graphics and updates, which are both missing
     for sprite in sprites:
         screen.blit(sprite.image, (sprite.rect.x, sprite.rect.y))
-    pygame.display.flip()
 
+    # -1: Why are you flipping the screen instead of presenting it?
+    pygame.display.flip()
+    
+    # Note: see previous note about delta time and it's placement
     clock.tick(60)  # limits FPS to 60
 
 pygame.quit()
